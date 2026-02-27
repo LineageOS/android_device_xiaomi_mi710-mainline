68c1f91875e5bac6e548b1885833d5a163bdb9bc
Yumi Yukimura <me.cafebabe@gmail.com>
2026-02-27 16:57:13 +0800

    mi710-mainline: Cleanup the remaining mi7150 bits
    Change-Id: I75e604c90d91fa30ec32aefba05edb38cad06100

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 PRODUCT_MAKEFILES := \
     aosp_pyxis_mainline:$(LOCAL_DIR)/pyxis_mainline/aosp_pyxis_mainline.mk \
     lineage_pyxis_mainline:$(LOCAL_DIR)/pyxis_mainline/lineage_pyxis_mainline.mk
-#    lineage_sweet_mainline:$(LOCAL_DIR)/sweet_mainline/lineage_sweet_mainline.mk
 
 $(foreach build_type, user userdebug eng, \
     $(eval COMMON_LUNCH_CHOICES += aosp_pyxis_mainline-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_pyxis_mainline-$(build_type)))
-#    $(eval COMMON_LUNCH_CHOICES += lineage_sweet_mainline-$(build_type)))
