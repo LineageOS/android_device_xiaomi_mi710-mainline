6c05bac0b6ee5cff4a0f4287075f344d849dbefb
Yumi Yukimura <me.cafebabe@gmail.com>
2026-02-27 16:45:06 +0800

    mi710-mainline: pyxis: Drop the obsolete davinci firmware setup
    Change-Id: I2c21cef7c1b42d6f1637636b15fdd4beb14a4c05

--- pyxis_mainline/device.mk
+++ pyxis_mainline/device.mk
@@ -27,20 +27,9 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2340
 
-# DSP
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(TARGET_DEVICE_PATH)/socinfo/,$(TARGET_COPY_OUT_VENDOR)/etc/hexagonrpcd-root/socinfo/) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/pyxis/proprietary/vendor/etc/acdbdata/,$(TARGET_COPY_OUT_VENDOR)/etc/hexagonrpcd-root/acdb/) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/pyxis/proprietary/vendor/etc/sensors/config/,$(TARGET_COPY_OUT_VENDOR)/etc/hexagonrpcd-root/sensors/config/) \
-    vendor/xiaomi/pyxis/proprietary/vendor/etc/sensors/sns_reg_config:$(TARGET_COPY_OUT_VENDOR)/etc/hexagonrpcd-root/sensors/sns_reg.conf
-
 # Firmware
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/pyxis/proprietary/vendor/firmware/a615_zap.elf:$(TARGET_COPY_OUT_ODM)/firmware/qcom/sdm710/xiaomi/pyxis/a615_zap.mbn
-
 PRODUCT_PACKAGES += \
-    all_symlink_firmware_pyxis \
-    firmware_pyxis_ipa_fws.mbn
+    all_symlink_firmware_pyxis
 
 # Init
 PRODUCT_PACKAGES += \
